2ff66165d9121030c2d02d790e27e35b9d3f5462
edd-ie <ogendoeddie@gmail.com>
2023-03-13 19:17:51 +0300

    Complete

---
+++ index.js
@@ -0,0 +1,15 @@
+//TODO: divide()
+//Todo: divide 2000 / 100
+function divide() {
+    return 2000/100;
+}
+
+
+//TODO: an  => square()
+//Todo: takes one parameter, multiplies times itself
+const square = num => num * num;
+
+
+//TODO: an => add()
+//Todo: two parameters, adds them together
+const add = (num1, num2) => num1 + num2;
